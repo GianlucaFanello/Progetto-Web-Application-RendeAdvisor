a68d3a10d0e84d51fc92fe9021d1290acdfa2684
Gianluca <fanello976@gmail.com>
2025-12-11 14:27:43 +0100

    aggiunto API mappa

---
+++ prova/src/app/home-page/home-page.js
@@ -0,0 +1,14 @@
+function MostraMappa() {
+  const centro = { lat: 39.3534, lng: 16.2273 };
+
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 14,
+    center: centro,
+  });
+
+  new google.maps.Marker({
+    position: centro,
+    map: map,
+    title: "Centro di Rende"
+  });
+}
