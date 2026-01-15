3cf18f05b234f7a147e5f39d70d2ad56f2065bb0
Bruno <212409209+BrunoStackDev@users.noreply.github.com>
2026-01-15 22:58:20 +0100

    Aggiunto config spring security

---
+++ RendeAdvisor_BE/src/main/java/it/unical/demacs/wa/rendeadvisor_be/config/SecurityConfig.java
@@ -0,0 +1,14 @@
+package it.unical.demacs.wa.rendeadvisor_be.config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration public class SecurityConfig {
+
+    @Bean public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth ->
+                        auth .requestMatchers("/api/utenti/registrazione").permitAll()
+                                .anyRequest().permitAll() );
+        return http.build(); } }
