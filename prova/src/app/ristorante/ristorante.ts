740c534c310135da3a3f9413932056d358b82140
brunolen <212409209+BrunoStackDev@users.noreply.github.com>
2026-01-16 19:48:43 +0100

    Sistemato ristorante search

--- prova/src/app/ristorante/ristorante.ts
+++ prova/src/app/ristorante/ristorante.ts
@@ -1,13 +1,29 @@
 import { Component } from '@angular/core';
-import {RouterLink} from '@angular/router';
+import {Router, RouterLink} from '@angular/router';
 import {FormsModule} from '@angular/forms';
+import {AttivitaService} from '../service/AttivitaService';
 
 @Component({
   selector: 'app-ristorante',
-  imports: [RouterLink, FormsModule],
+  imports: [FormsModule],
   templateUrl: './ristorante.html',
   styleUrl: './ristorante.css',
 })
 export class Ristorante {
+
+  searchText: string = '';
+
+  constructor(
+    private router: Router,
+    private attivitaService: AttivitaService
+  ) { }
+
+  search() {
+    if (this.searchText.trim()) {
+      this.router.navigate(['/RisultatiR'], {
+        queryParams: { query: this.searchText }
+      });
+    }
+  }
 
 }
