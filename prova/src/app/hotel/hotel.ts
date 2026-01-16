63369a59f01de2a89519eee8d347e676854d0e5b
Gianluca <fanello976@gmail.com>
2026-01-16 19:35:03 +0100

    collegamento searc hotel e ristorante

--- prova/src/app/hotel/hotel.ts
+++ prova/src/app/hotel/hotel.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
-import {RouterLink} from '@angular/router';
+import {Router, RouterLink} from '@angular/router';
+import {FormsModule} from '@angular/forms';
+import {AttivitaService} from '../service/AttivitaService';
 
 @Component({
   selector: 'app-hotel',
-  imports: [RouterLink],
+  imports: [RouterLink, FormsModule],
   templateUrl: './hotel.html',
   styleUrl: './hotel.css',
 })
 export class Hotel {
+
+  searchText: string = '';
+
+  constructor(
+    private router: Router,
+    private attivitaService: AttivitaService
+  ) { }
+
+  search() {
+    if (this.searchText.trim()) {
+      this.router.navigate(['/RisultatiR'], {
+        queryParams: { query: this.searchText }
+      });
+    }
+
+  }
 
 }
--- prova/src/app/ristorante/ristorante.ts
+++ prova/src/app/ristorante/ristorante.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import {RouterLink} from '@angular/router';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-ristorante',
-  imports: [RouterLink],
+  imports: [RouterLink, FormsModule],
   templateUrl: './ristorante.html',
   styleUrl: './ristorante.css',
 })
