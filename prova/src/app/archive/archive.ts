89a947e0cc4785e478e190544518de54d0e843d7
brunolen <212409209+BrunoStackDev@users.noreply.github.com>
2026-02-06 02:02:23 +0100

    Aggiunta stelle in archivio

--- prova/src/app/archive/archive.ts
+++ prova/src/app/archive/archive.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { AttivitaService } from '../service/AttivitaService';
 import { AttivitaDto } from '../model/attivita.dto';
 import { Router } from '@angular/router';
+import {RecensioneService} from '../service/RecensioneService';
 
 @Component({
   selector: 'app-archive',
@@ -19,7 +20,8 @@ export class Archive implements OnInit {
 
   constructor(
     private attivitaService: AttivitaService,
-    private router: Router
+    private router: Router,
+    private recensioneService: RecensioneService
   ) {}
 
   ngOnInit(): void {
@@ -30,8 +32,18 @@ export class Archive implements OnInit {
 
     this.attivitaService.getHotel().subscribe({
       next: (res) => {
-        // Usa res.data se il backend risponde con ApiResponse, altrimenti adatta
         this.listaHotel = res.data || [];
+
+        this.listaHotel.forEach(
+          hotel => {
+            this.recensioneService.getRatingStruttura(hotel.nomeLocale).subscribe(
+              res => {
+                (hotel as any).rating = res.data
+                this.listaHotel = [...this.listaHotel];
+                console.log("Rating per", hotel.nomeLocale, "=", res.data);
+              }
+            );
+          });
       },
       error: () => console.error('Errore caricamento hotel')
     });
@@ -39,6 +51,15 @@ export class Archive implements OnInit {
     this.attivitaService.getRistoranti().subscribe({
       next: (res) => {
         this.listaRistoranti = res.data || [];
+
+        this.listaRistoranti.forEach(
+          rist => {
+            this.recensioneService.getRatingStruttura(rist.nomeLocale).subscribe(
+              res => {
+                (rist as any).rating = res.data;
+                this.listaRistoranti = [...this.listaRistoranti]
+              })
+          })
         this.loading = false;
       },
       error: () => this.loading = false
@@ -54,4 +75,19 @@ export class Archive implements OnInit {
   visualizzaRecensioni(nomeLocale: String) {
     this.router.navigate(['/recensioni-struttura',nomeLocale]);
   }
+
+  getStarSrc(i:number, attivita: AttivitaDto) {
+    const rating = (attivita as any).rating;
+
+    if (rating === undefined || rating === null) {
+      return '/assets/star-empty.png'; // fallback temporaneo
+    }
+
+    const full = Math.floor(rating);
+    const half = rating % 1 >= 0.5;
+
+    if (i < full) return '/assets/star-full.png';
+    if (i === full && half) return '/assets/star-half.png';
+    return '/assets/star-empty.png';
+    }
 }
